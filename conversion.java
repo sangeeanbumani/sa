99ff2628889f3873162e082bb3191ddb6fb942d5
sangeeanbumani <sangeepavi143@gmail.com>
2016-09-07 12:30:56 +0530

    Update conversion.java

--- conversion.java
+++ conversion.java
@@ -2,3 +2,11 @@ import java.io.*;
 class conversion
 {
 public satic void main(String args[])
+{
+  Scanner s=new Scanner(System.in);
+  int a=s.nextInt();
+  String b=Integer.toString(a);
+  System.out.println("b");
+  
+}
+}
