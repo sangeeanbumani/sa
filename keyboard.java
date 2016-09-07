1c9f40adb3577fb0a263661acc12ba2747620a93
sangeeanbumani <sangeepavi143@gmail.com>
2016-09-07 12:09:25 +0530

    Create keyboard.java

---
+++ keyboard.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public static void main(String args[])
+{
+class keyboard
+{
+Scanner s=new Scanner(System.in);
+char key=s.nextChar();
+if(key=='Q')
+{
+exit(0);
+}
+else
+continue;
+}
+}
