22a209c97f6ec18beb05b2ae86d2b5d79e792e2e
sangeeanbumani <sangeepavi143@gmail.com>
2016-09-07 12:26:57 +0530

    Update keyboard.java

--- keyboard.java
+++ keyboard.java
@@ -1,7 +1,7 @@
 import java.util.*;
-public static void main(String args[])
-{
 class keyboard
+{
+public static void main(String args[])
 {
 Scanner s=new Scanner(System.in);
 char key=s.nextChar();
